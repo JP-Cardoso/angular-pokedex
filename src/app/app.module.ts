525a0f98751eadeac5bc4df527f6821ca6d5c2f4
JP-Cardoso <jhonpoal@hotmail.com>
2023-09-06 11:30:54 -0300

    feat: api service I

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
 //modules
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
 
-//componentes
 
 @NgModule({
   declarations: [
@@ -17,7 +16,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     PagesModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
 
 @Component({
   selector: 'app-poke-list',
   templateUrl: './poke-list.component.html',
   styleUrls: ['./poke-list.component.scss']
 })
-export class PokeListComponent {
+export class PokeListComponent implements OnInit {
   public readonly pokemonImage: string = "../../../assets/pokemon/pokemon.png";
+
+  constructor(
+    private apiService: PokeApiService
+  ) {
+
+  };
+
+  ngOnInit(): void {
+    this.apiService.apiListAllPaokemons.subscribe(res => res);             
+  };
+
+
 }
