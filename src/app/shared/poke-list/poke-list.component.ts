b2de554063f10af842a569634f35e0b55ca64b33
JP-Cardoso <jhonpoal@hotmail.com>
2023-09-06 11:09:38 -0300

    feat: config poke-list component

--- src/app/shared/poke-list/poke-list.component.ts
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./poke-list.component.scss']
 })
 export class PokeListComponent {
-
+  public readonly pokemonImage: string = "../../../assets/pokemon/pokemon.png";
 }
